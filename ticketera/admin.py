4b98b24e6634d3172871ae6e32aa6cf30c440b35
Barrera Ramiro <ramiro.n.barrera@gmail.com>
2022-12-11 12:08:49 -0300

    Agregado de modelos a Django Admin + idioma es-AR

--- ticketera/admin.py
+++ ticketera/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from ticketera.models import Usuario, Empresa, Ticket
+
 # Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Empresa)
+admin.site.register(Ticket)
