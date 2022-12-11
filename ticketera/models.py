4b46793fd93efdb7a732e28c53f540df7d9c39f3
unknown <pbonaduce@iiconcepcion.edu.ar>
2022-12-10 23:02:41 -0300

    models.ManyToMany

--- ticketera/models.py
+++ ticketera/models.py
@@ -3,31 +3,55 @@ from django.db import models
 # Create your models here.
 
 class Usuario (models.Model):
-    nombre_usuario = models.CharField(max_length=50)
-    apellido_usuario = models.CharField(max_length=50)
-    email_usuario = models.EmailField(max_length=50)
-    telefono_usuario = models.CharField(max_length=50)
-    nombre_empresa = models.CharField(max_length=50)
-    contraseña_usuario = models.CharField(max_length=50) #(label="Password", widget=forms.PasswordInput, strip=False)     
+    nombre_usuario = models.CharField(max_length=50, verbose_name="Nombre")
+    apellido_usuario = models.CharField(max_length=50, verbose_name="Apellido")
+    email_usuario = models.EmailField(max_length=50, verbose_name="Email")
+    telefono_usuario = models.CharField(max_length=50, verbose_name="Teléfono")
+    nombre_empresa = models.CharField(max_length=50, verbose_name="Empresa")
+    usuario = models.CharField(max_length=50, verbose_name="Usuario")
+    contraseña_usuario = models.CharField(max_length=50, verbose_name="Contraseña")#(label="Password", widget=forms.PasswordInput, strip=False)
+    baja = models.BooleanField(default=False)
+      
     def __str__(self):
         return self.nombre_usuario
     
+    def soft_delete(self):
+        self.baja = True
+        self.save()
+        
+    def restore(self):
+        self.baja = False
+        self.save()
+        
+    
 class Empresa   (models.Model):
-    nombre_empresa = models.CharField(max_length=50)
-    direccion_empresa = models.CharField(max_length=50)
-    telefono_empresa = models.CharField(max_length=50)
-    email_empresa = models.EmailField(max_length=50)
+    nombre_empresa = models.CharField(max_length=50, verbose_name="Nombre de la empresa")
+    direccion_empresa = models.CharField(max_length=50, verbose_name="Dirección de la empresa")
+    telefono_empresa = models.CharField(max_length=50, verbose_name="Teléfono de la empresa")
+    email_empresa = models.EmailField(max_length=50, verbose_name="Email de la empresa")
+    baja = models.BooleanField(default=False)
+      
     def __str__(self):
         return self.nombre_empresa
+    
+    def soft_delete(self):
+        self.baja = True
+        self.save()
+        
+    def restore(self):
+        self.baja = False
+        self.save()
+
 
 class Ticket (models.Model):
-    titulo = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=50)
-    prioridad = models.CharField(max_length=50)
-    estado = models.CharField(max_length=50)
-    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
-    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE)
-    fecha_creacion = models.DateField()
-    fecha_cierre = models.DateField()    
+    titulo = models.CharField(max_length=50, verbose_name="Título")
+    descripcion = models.CharField(max_length=50, verbose_name="Descripción")
+    prioridad = models.CharField(max_length=50, verbose_name="Prioridad")
+    estado = models.CharField(max_length=50, verbose_name="Estado")
+    fecha_creacion = models.DateField(verbose_name="Fecha de creación")
+    fecha_cierre = models.DateField(verbose_name="Fecha de cierre") 
+    usuario = models.ManyToManyField(Usuario, verbose_name="Usuario")
+    empresa = models.ForeignKey(Empresa, on_delete=models.CASCADE, verbose_name="Empresa")
+    
     def __str__(self):
         return self.titulo
